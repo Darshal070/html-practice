ea830243c7a3d4d943cd333f69cf281a539656e2
Darshal <dhadgedarshal@gmail.com>
2026-09-18 17:19:52 +0530

    Add files via upload

---
+++ pr77.js
@@ -0,0 +1,13 @@
+document.write("Welcome..!!");
+console.log("Hello Javascript console");
+alert("Error");
+confirm("success");
+
+let name = prompt("Enter name");
+let dept=prompt("Enter your department")
+document.write("<br>");
+document.write(name);
+document.write("<br>");
+document.write(dept)
+console.log(name);
+
